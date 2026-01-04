c314af1391c1cedc124bc4e435536c863ea2029e
J-hassan <imuhammadhassanktk@gmail.com>
2026-01-04 14:40:46 +0500

    Created Grid for Tetris

---
+++ src/grid.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<vector>
+#include "raylib.h"
+
+class Grid
+{
+public:
+    Grid(); // consturctor - it has same name as class 
+    void initialize(); // this function initialises a grid by setting all the rows and cols to value -> 0
+    void print(); // just for the checking purpose
+    int grid[20][10]; // it stores the value for each cell
+    void Draw(); // this function draws the grid by setting respective color to each grid
+private:
+    int numCols; // it defines how many cols the grid will have
+    int numRows; // it defines how many rows the grid will have
+    int cellSize; // it defines the size for each cell
+    std::vector<Color> getCellColors(); // this function defines all the colors and return them as a vector
+    std::vector<Color> colors; // this vector stores all the colors and can be accessed as -> colors[index]
+};
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,24 @@
 #include <raylib.h>
-#include "ball.h"
+#include "grid.h"
 
 int main() 
 {
-    const Color darkGreen = {20, 160, 133, 255};
+    const Color darkBlue = {44, 44, 127, 255}; // this method is given by the raylib library for defining colors 
+        
+    InitWindow(300, 600, "Tetris By Developers"); // initalizing a window having width of 300 and height of 600 and title 
+    SetTargetFPS(60); // setting fps so that it runs with same speed no every device
+
+    Grid grid = Grid(); // creating a grid object 
+    grid.print(); // printing in terminal to see if rows and cols are printed correctly
     
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
+    while (WindowShouldClose() == false) // this checks if close button or escape button is pressed and window is closed
     {
-        ball.Update();
         
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+
+            ClearBackground(darkBlue); // making the background darkBlue - as a default background color
+            grid.Draw(); // drawing the grid using rectangles
+
         EndDrawing();
     }
     
